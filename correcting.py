6e8e9fdacd13b55ac6609dcd4808af955c5e233f
Vyacheslav Mitin <vyacheslav.mitin@gmail.com>
2022-01-19 04:18:55 +0400

    коррекция

--- correcting.py
+++ correcting.py
@@ -5,13 +5,10 @@ import pyautogui as pg
 import pyperclip
 import time
 
+from dates import TUPLE_DATES, DATE
 
 # Константы
-TIMES = 50
-
-
-LIST_ = [
-'01.04.22','30.06.22','01.03.22','01.04.22','01.02.22','01.04.22','01.03.22','01.04.22','05.03.22','01.01.23','31.10.22','01.06.22','01.01.22','04.04.22','01.09.22','05.03.22','01.06.22','05.03.22','30.11.22','31.07.22','01.11.22','01.01.23','01.02.22','01.04.22','01.04.22','01.08.22','01.01.23','30.09.22','01.12.22','01.02.22','01.11.22','15.03.22','21.07.22','01.05.22','01.06.22','01.05.22']
+TIMES = 10000
 
 
 # Функции
@@ -33,20 +30,26 @@ def correcting_contracts():
     pg.press('right', presses=7)
 
     for i in range(TIMES):
-        pg.press('down')
-        time.sleep(0.1)
+        time.sleep(0.0)
         pg.hotkey("ctrl", "c")
-        if pyperclip.paste() in LIST_:
-            pg.press('enter')
+        if pyperclip.paste() in TUPLE_DATES:
+            pg.press('enter')  # вход в договор
             time.sleep(1)
-            pg.click(650, 400)
+            pg.click(650, 400)  # клик на поле с датой конца периода
             pg.press('backspace', presses=10)
-            pg.write('30.06.25')
-            time.sleep(1)
-            pg.click(70, 725)
-            time.sleep(1)
-            pg.press('space')
-            time.sleep(3)
+            pg.write(DATE)
+            time.sleep(0.5)
+            pg.click(70, 725)  # клик на кнопке сохранить
+            time.sleep(0.2)
+            pg.click(1220, 760)  # клик на кнопке пересчета даты
+            time.sleep(0.2)
+            pg.click(1220, 760)  # клик на всякий случай если договор дубль есть
+            pg.click(2140, 230)  # клик на столбце и первой строке в окон. периоде в журнале
+            # break
+            # time.sleep(1)
+            # pg.press('space')
+            # time.sleep(3)
+            # pg.press('down', presses=i+1)
             # pg.press('tab', presses=2, interval=0.5)
             # pg.hotkey("ctrl", "c")
             # if pyperclip.paste() == 'Нет' or 'Да':
@@ -70,6 +73,8 @@ def correcting_contracts():
             #     pg.press('esc')
             # pg.press('tab', presses=5, interval=0.5)
             # input()
+        else:
+            pg.press('down')
 
 
 if __name__ == '__main__':
